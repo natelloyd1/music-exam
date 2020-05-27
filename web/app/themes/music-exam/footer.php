cd220dca22a6812b9f686e89512f6f6d62688079
natelloyd1 <nathanaellloyd@live.com>
2020-05-27 12:07:50 +0100

    set up carousel, although having styling issues with it

--- web/app/themes/music-exam/footer.php
+++ web/app/themes/music-exam/footer.php
@@ -1,20 +1,23 @@
-<footer>
-    <div class="container">
-            <?php 
+        <footer>
+            <div class="container">
+                    <?php 
 
-                wp_nav_menu(
+                        wp_nav_menu(
 
-                    array(
-                        'theme_location' => 'footer-menu',
-                        'menu_class' => 'footer-bar'
-                    )
-                )
-            ?>
+                            array(
+                                'theme_location' => 'footer-menu',
+                                'menu_class' => 'footer-bar'
+                            )
+                        )
+                    ?>
 
-            <?php wp_footer(); ?>
-    </div>
-</footer>
+                    <?php wp_footer(); ?>
+            </div>
+        
+        </footer>
+        
     <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script> 
+    
     </body>
 </html>
--- web/app/themes/music-exam/includes/section-content.php
+++ web/app/themes/music-exam/includes/section-content.php
@@ -28,7 +28,31 @@
 
         <?php
             $creative_united = get_field('creative_united'); 
+            $icmp = get_field('icmp'); 
+            $orange = get_field('orange'); 
+            $orange_amp = get_field('orange_amp');
+            $youth_music = get_field('youth_music');
         ?>
+
+    <div class="carousel-wrap">
+        <div class="carousel" data-flickity='{ "fullscreen": true, "lazyLoad": 2, "contain": true, "pageDots": false }'>
+            <div class="gallery-cell">
+                <img class="carousel-image" src="<?php echo esc_url($creative_united['url']); ?>" alt="<?php echo esc_attr($creative_united['alt']); ?>" />
+            </div>
+            <div class="gallery-cell">
+                <img class="carousel-image" src="<?php echo esc_url($icmp['url']); ?>" alt="<?php echo esc_attr($icmp['alt']); ?>" />
+            </div>
+            <div class="gallery-cell">
+                <img class="carousel-image" src="<?php echo esc_url($orange['url']); ?>" alt="<?php echo esc_attr($orange['alt']); ?>" />
+            </div>
+            <div class="gallery-cell">
+                <img class="carousel-image" src="<?php echo esc_url($orange_amp['url']); ?>" alt="<?php echo esc_attr($orange_amp['alt']); ?>" />
+            </div>
+            <div class="gallery-cell">
+                <img class="carousel-image" src="<?php echo esc_url($youth_music['url']); ?>" alt="<?php echo esc_attr($youth_music['alt']); ?>" />
+            </div>
+        </div>
+    </div>    
 
     <!-- <img class="creative-united" src="<?php echo esc_url($creative_united['url']); ?>" alt="<?php echo esc_attr($creative_united['alt']); ?>" /> -->
 
